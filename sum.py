489d2d34c93384a71ab16782d3f018f33846637e
tarun30598 <59306048+tarun30598@users.noreply.github.com>
2019-12-28 14:51:06 +0530

    Add files via upload

--- sum.py
+++ sum.py
@@ -1,6 +1,10 @@
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
- 
-sum = a + b
- 
-print("sum:", sum)
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
